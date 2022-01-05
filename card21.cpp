dbf4bc46db92678b958886c33247bbfa0b0c6dfc
Sulaimancrypted <64346270+Sulaimancrypted@users.noreply.github.com>
2022-01-05 14:55:47 +0530

    21 Card trick simulation using C++

--- card21.cpp
+++ card21.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main()
 {
     int x[21] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
-    int y[21],j;
+    int y[21];
+    int j;
     for (int i = 0; i < 21; i++)
     {
         cout << x[i] << "\t" << x[++i] << "\t" << x[++i] << "\n";
